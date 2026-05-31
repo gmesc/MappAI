24ba1f0b0b5537f70c0ed85c56fbdaa2ce3bdcfc
gmesc <giacomomeschini@gmail.com>
2026-05-31 23:26:38 +0200

    WIP: auto-save 2026-05-31 23:26

--- parse_drawer.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('public/index_classi_nuove.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-drawer = re.search(r'<div id="insegnai-drawer".*?<!-- Fine Insegnai\.ch Drawer -->', html, re.DOTALL)
-if drawer:
-    print(drawer.group(0))
